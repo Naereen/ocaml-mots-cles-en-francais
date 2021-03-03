dc1fada78716cadcecd60c23cc76f99c2876312c
Lilian Besson <lilian.besson@crans.org>
2021-03-03 17:28:16 +0100

    Adding my two baby examples in English OCaml.

---
+++ example_fact.ml
@@ -0,0 +1,36 @@
+(**
+ * A small OCaml (https://OCaml.org) program, written with keywords in English,
+ * It defines a recursive factorial function, and run some test on it.
+ *
+ * See <https://github.com/Naereen/ocaml-mots-cles-en-francais/>
+ *
+ * Copyright Lilian Besson, 2021, released under the MIT License
+ * See <https://lbesson.mit-license.org/>
+*)
+
+let rec fact (n : int) =
+  match n with
+  | n when n < 0 -> failwith "Error: n has to be >= 0 for fact."
+  | 0 -> 1
+  | 1 | 2 -> if n=1 then 1 else 2
+  | n -> n * (fact (n-1))
+;;
+
+let print = Format.printf;;
+
+print "Test with fact written with the keywords in English! (default mode)";;
+assert (1 = (fact 1));;
+assert (2 = (fact 2));;
+assert (6 = (fact 3));;
+for n = 6 to 10 do
+  print "fact(%i) = %i\n%!" n (fact n);
+done;;
+for n = 13 downto 11 do
+  print "fact(%i) = %i\n%!" n (fact n);
+done;;
+
+let n = ref 3 in
+while !n < 6 do
+  print "fact(%i) = %i\n%!" !n (fact !n);
+  incr n;
+done;;
---
+++ example_keywords.ml
@@ -0,0 +1,35 @@
+(**
+ * A small OCaml (https://OCaml.org) program, written with keywords in English,
+ * It does a lot of basic OCaml stuff, and run some tests on them.
+ *
+ * See <https://github.com/Naereen/ocaml-mots-cles-en-francais/>
+ *
+ * Copyright Lilian Besson, 2021, released under the MIT License
+ * See <https://lbesson.mit-license.org/>
+*)
+
+let print = Format.printf;;
+
+print "Test almost all other English keywords...\n%!";;
+
+let x:int = try begin 2021/0 end with _ -> 42;;
+assert (x = 42);;
+
+let y = 40
+and z = 2
+in begin
+  let answertolife = y + z in
+  assert (answertolife = 42)
+end;;
+
+assert true;;
+assert (not false);;
+assert (true || false);;
+assert (true or false);;  (* Deprecated *)
+assert (true && (not false));;
+(* 'and' is for another purpose *)
+
+open List;;
+assert ((length [1;2;3]) = 3);;
+
+print "Done for the tests in English!\n%!";;
